f3dd27a9bf86dd79cab4be4e24cfe882909aac85
Nguyen Vu <phucnguyenvu0203@gmail.com>
2023-11-19 23:37:30 -0800

    Added farm lane, crop, water/sun

--- Assets/FarmGrid.cs
+++ Assets/FarmGrid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FarmGrid : MonoBehaviour
 {
-    public GameObject cellPrefab;
+    public GameObject gridCellPrefab;
     public int gridSize = 4;
     public float originalLocation = -1.5f;
     public float cellSize = 1.0f;
@@ -21,7 +21,15 @@ public class FarmGrid : MonoBehaviour
             for (float y = originalLocation; y < originalLocation+gridSize; y++)
             {
                 Vector3 cellPosition = new Vector3(x * cellSize, y * cellSize, 0);
-                Instantiate(cellPrefab, cellPosition, Quaternion.identity, transform);
+                GameObject cellObject = Instantiate(gridCellPrefab, cellPosition, Quaternion.identity, transform);
+
+                // Attach the GridCell script to the instantiated cell
+                GridCell gridCell = cellObject.GetComponent<GridCell>();
+                if (gridCell != null)
+                {
+                    // Use InitializeCell instead of ChangeCellContent during initialization
+                    gridCell.InitializeCell();
+                }
             }
         }
     }
---
+++ Assets/GridCell.cs
@@ -0,0 +1,105 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GridCell : MonoBehaviour
+{
+    public enum PlantType
+    {
+        None,
+        Grass,
+        Carrot
+    }
+
+    public enum GrowthLevel
+    {
+        Seed,
+        Grown
+    }
+
+    public SpriteRenderer spriteRenderer;
+
+    public bool water = false;
+    public bool sun = false;
+    public PlantType plantType;
+    public GrowthLevel growthLevel;
+
+void Start()
+{
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    if (spriteRenderer == null)
+    {
+        Debug.LogError("SpriteRenderer component not found on GridCell prefab.");
+        return;
+    }
+
+    InitializeCell();
+}
+
+
+    public void InitializeCell()
+    {
+        // Set initial values
+        water = Random.Range(0f, 1f) > 0.5f;
+        sun = Random.Range(0f, 1f) > 0.5f;
+        plantType = (PlantType)Random.Range(0, System.Enum.GetValues(typeof(PlantType)).Length);
+        growthLevel = GrowthLevel.Seed; // Set initial growth level to seed
+
+        // Update visual representation based on the initial values
+        UpdateCellVisuals();
+    }
+
+    void UpdateCellVisuals()
+    {
+        // Adjust the color or sprite based on the current content
+        if (plantType == PlantType.Grass)
+        {
+            if (growthLevel == GrowthLevel.Seed)
+            {
+                spriteRenderer.color = new Color(0f, 1f, 0f, 0.5f); // Light green for seed
+            }
+            else
+            {
+                spriteRenderer.color = Color.green; // Full green for grown
+            }
+        }
+        else if (plantType == PlantType.Carrot)
+        {
+            if (growthLevel == GrowthLevel.Seed)
+            {
+                spriteRenderer.color = new Color(1f, 0.647f, 0f, 0.5f); // Light orange for seed
+            }
+            else
+            {
+                spriteRenderer.color = new Color(1f, 0.647f, 0f); // Full orange for grown
+            }
+        }
+        else if (water && sun)
+        {
+            spriteRenderer.color = new Color(0.545f, 0.271f, 0.075f); // Brown color
+        }
+        else if (water)
+        {
+            spriteRenderer.color = Color.blue;
+        }
+        else if (sun)
+        {
+            spriteRenderer.color = Color.yellow;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    // Example method to change the content of the cell
+    public void ChangeCellContent(bool newWater, bool newSun, PlantType newPlantType, GrowthLevel newGrowthLevel)
+    {
+        water = newWater;
+        sun = newSun;
+        plantType = newPlantType;
+        growthLevel = newGrowthLevel;
+
+        UpdateCellVisuals();
+    }
+}
